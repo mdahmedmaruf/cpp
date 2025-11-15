d71f4bf65c0e33e96083b2feb672465ffcc56104
Maruf Ahmed <mdahmedmaruf@gmail.com>
2025-11-15 23:31:23 +0600

    second file

---
+++ A_Way_Too_Long_Words.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    string word;
+
+    while (n--)
+    {
+        cin >> word;
+        if(word.size() > 10)
+            cout << word[0] << word.size() - 2 << word.back() << endl;
+        else
+            cout << word << endl;
+    }
+
+    return 0;
+}
