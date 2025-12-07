e80388cc77f8b86d20a3d25154b5d872a19bafea
Maruf Ahmed <mdahmedmaruf@gmail.com>
2025-12-07 20:15:19 +0600

    marathon

---
+++ A_Marathon.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t, a, b, c, d;
+    cin >> t;
+
+    while (t--)
+    {
+        cin >> a >> b >> c >> d;
+        cout << (b > a) + (c > a) + (d > a) << '\n';
+    }
+
+    return 0;
+}
