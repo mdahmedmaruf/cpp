d09168e67a1e4082d72ed13389e9d7cd4b109a13
Maruf Ahmed <mdahmedmaruf@gmail.com>
2025-12-07 19:44:43 +0600

    buy shovel

---
+++ A_Buy_a_Shovel.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int k, r;
+    cin >> k >> r;
+
+    for (int i = 1; i <= 10; i++)
+    {
+        int cost = k * i;
+        if (cost % 10 == 0 || cost % 10 == r)
+        {
+            cout << i << '\n';
+            break;
+        }
+    }
+
+    return 0;
+}
