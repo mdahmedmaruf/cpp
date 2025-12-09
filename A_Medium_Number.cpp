104573c21420d3f40094867929a1135aea5b02f0
Maruf Ahmed <mdahmedmaruf@gmail.com>
2025-12-09 09:09:12 +0600

    medium number

---
+++ A_Medium_Number.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int v[3];
+        cin >> v[0] >> v[1] >> v[2];
+        sort(v, v + 3);
+        cout << v[1] << endl;
+    }
+
+    return 0;
+}
