1653e68b9251ffd08d6319c0788ebb9418401d07
Maruf Ahmed <mdahmedmaruf@gmail.com>
2025-12-10 20:17:07 +0600

    to my critics

---
+++ A_To_My_Critics.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t, a, b, c;
+    cin >> t;
+    while (t--)
+    {
+        cin >> a >> b >> c;
+        if (a + b >= 10 || b + c >= 10 || c + a >= 10)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    return 0;
+}
