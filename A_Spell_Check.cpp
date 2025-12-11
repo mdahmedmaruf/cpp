e16914b54b2a5d94a63a4a5dc5a639a4205d642c
Maruf Ahmed <mdahmedmaruf@gmail.com>
2025-12-11 11:09:55 +0600

    spell check

---
+++ A_Spell_Check.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t, n;
+    cin >> t;
+    string s;
+    while (t--)
+    {
+        cin >> n >> s;
+        sort(s.begin(), s.end());
+        cout << (s == "Timru" ? "YES\n" : "NO\n");
+    }
+
+    return 0;
+}
