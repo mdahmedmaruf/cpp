64dd37529da5550d24999d35c02f80244b57f583
Maruf Ahmed <mdahmedmaruf@gmail.com>
2025-12-16 02:48:36 +0600

    short substrings

---
+++ A_Short_Substrings.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        cin >> s;
+        for (int i = 0; i < (int)s.size(); i += 2)
+            cout << s[i];
+
+        cout << s.back() << endl;
+    }
+
+    return 0;
+}
