ab009757d4c99ede86c6acacf10621555ee805e6
Maruf Ahmed <mdahmedmaruf@gmail.com>
2025-12-19 20:26:13 +0600

    prime function

---
+++ D_Prime_Function.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        if (n == 1)
+        {
+            cout << "NO" << endl;
+            continue;
+        }
+
+        bool flag = true;
+        for (int i = 2; i * i <= n; i++)
+        {
+            if (n % i == 0)
+            {
+                flag = false;
+                break;
+            }
+        }
+
+        if (flag)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    return 0;
+}
