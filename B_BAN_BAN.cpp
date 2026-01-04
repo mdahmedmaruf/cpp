8b09d8f8782e69a4c52747aa64a9bcbb133641b2
Maruf Ahmed <mdahmedmaruf@gmail.com>
2026-01-04 18:00:53 +0600

    ban ban

---
+++ B_BAN_BAN.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        cout << n / 2 + n % 2 << "\n";
+        int l = 1, r = 3 * n;
+        while (l < r)
+        {
+            cout << l << " " << r << endl;
+            l += 3;
+            r -= 3;
+        }
+    }
+
+    return 0;
+}
