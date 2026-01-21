516852175dd29eecc2b162bb8ed4613e785642a6
Maruf Ahmed <mdahmedmaruf@gmail.com>
2026-01-21 20:24:39 +0600

    doremy paint

---
+++ A_Doremy_s_Paint_3.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        map<int, int> mp;
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+            mp[a[i]]++;
+        }
+
+        if (mp.size() > 2)
+            cout << "No" << endl;
+        else
+        {
+            if (mp.size() == 2)
+            {
+                vector<int> v;
+                for (auto p : mp)
+                    v.push_back(p.second);
+                if (abs(v[0] - v[1]) > 1)
+                    cout << "No" << endl;
+                else
+                    cout << "Yes" << endl;
+            }
+            else
+                cout << "Yes" << endl;
+        }
+    }
+
+    return 0;
+}
