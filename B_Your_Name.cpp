5e98fb0f917df61254829e068d23cfd55e597292
Maruf Ahmed <mdahmedmaruf@gmail.com>
2026-01-22 10:33:24 +0600

    harder problem

--- B_Your_Name.cpp
+++ B_Your_Name.cpp
@@ -3,13 +3,35 @@ using namespace std;
 
 int main()
 {
-    int t;
-    cin >> t;
-    while (t--)
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int q;
+    cin >> q;
+    while (q--)
     {
-        long long n, m, q;
-        cin >> n >> m >> q;
+        int n;
+        cin >> n;
+        string s, t;
+        cin >> s >> t;
+
+        // Quick length check
+        if (s.size() != t.size())
+        {
+            cout << "NO\n";
+            continue;
         }
 
+        array<int, 26> freqS = {0}, freqT = {0};
+        for (char c : s)
+            freqS[c - 'a']++;
+        for (char c : t)
+            freqT[c - 'a']++;
+
+        if (freqS == freqT)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
     return 0;
 }
